8014e2e0b353fb351ebc19be256040c85416c57a
Enrico Schwendig <enrico.schw@gmx.de>
2023-12-16 09:47:58 +0100

    stop tracking stats when connection closed

--- internal/rtp/stats/worker.go
+++ internal/rtp/stats/worker.go
@@ -6,12 +6,14 @@ import (
 	"github.com/pion/interceptor/pkg/stats"
 	"github.com/pion/webrtc/v3"
 	"github.com/shigde/sfu/internal/metric"
+	"golang.org/x/exp/slog"
 )
 
 func worker(labels metric.Labels, ssrc webrtc.SSRC, statsGetter stats.Getter, cancel <-chan struct{}) {
 	for {
 		select {
 		case <-cancel:
+			slog.Debug("stats.worker: close", "label", labels)
 			metric.CleanTrackStats(labels)
 			return
 		case <-time.After(5 * time.Second):
