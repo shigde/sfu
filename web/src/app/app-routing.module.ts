ccc43116ebce72653941a450b7fecd0b158f78dd
Enrico Schwendig <enrico.schw@gmx.de>
2024-09-29 20:03:14 +0200

    Move pages in own folder

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {LiveStreamComponent} from './live-stream/live-stream.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {UserRouteAccessGuard} from './guards/user-route-access.guard';
-import {LoginComponent} from './login/login.component';
-import {LobbyEntryComponent} from './lobby-entry/lobby-entry.component';
+import {LoginComponent} from './pages/login/login.component';
+import {LobbyEntryComponent} from './pages/lobby-entry/lobby-entry.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent, canActivate: [UserRouteAccessGuard] },
   { path: 'lobby/:spaceId/stream/:streamId', component: LobbyEntryComponent, canActivate: [UserRouteAccessGuard] },
-  { path: 'stream/:id', component: LiveStreamComponent, canActivate: [UserRouteAccessGuard] },
   { path: 'login', component: LoginComponent}
 ];
 
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,22 +3,20 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-import {LiveStreamComponent} from './live-stream/live-stream.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {LoginComponent} from './login/login.component';
+import {LoginComponent} from './pages/login/login.component';
 import {CommonModule} from '@angular/common';
 import {httpInterceptorProviders, ShigModule} from '@shigde/core';
-import {LobbyEntryComponent} from './lobby-entry/lobby-entry.component';
+import {LobbyEntryComponent} from './pages/lobby-entry/lobby-entry.component';
 import {SettingsComponent} from './svg/settings.component';
 
 
 @NgModule({ declarations: [
         AppComponent,
         DashboardComponent,
-        LiveStreamComponent,
         LoginComponent,
         LobbyEntryComponent,
         SettingsComponent
