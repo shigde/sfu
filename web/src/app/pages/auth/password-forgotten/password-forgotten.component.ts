80f19ad24c677c43c8801cfb00bbec9e5828481f
Enrico Schwendig <enrico.schw@gmx.de>
2024-09-30 21:50:23 +0200

    Add fix sign up and set css

--- web/src/app/pages/auth/password-forgotten/password-forgotten.component.ts
+++ web/src/app/pages/auth/password-forgotten/password-forgotten.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {SessionService} from '../../../../../../../shig-js-sdk/dist/core';
 
 @Component({
   selector: 'app-password-forgotten',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './password-forgotten.component.html',
   styleUrl: './password-forgotten.component.scss'
 })
 export class PasswordForgottenComponent {
+  protected passForgetForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
+  constructor(private router: Router, private session: SessionService) {
+  }
+
+  onSubmit() {
+    if (this.passForgetForm.valid) {
+      console.log(this.passForgetForm.value);
+    }
+  }
 }
