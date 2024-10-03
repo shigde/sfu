c51fe4772d2df5c3c64c209eecaf5e94d45196d6
Enrico Schwendig <enrico.schw@gmx.de>
2024-10-04 00:00:04 +0200

    add delete account endpoint

--- internal/auth/router.go
+++ internal/auth/router.go
@@ -9,7 +9,8 @@ func UseRoutes(router *mux.Router, accountService *AccountService) {
 	router.HandleFunc("/authenticate", handler.Authentication(accountService)).Methods("POST")
 	router.HandleFunc("/auth/login", handler.Login(accountService)).Methods("POST")
 	router.HandleFunc("/auth/register", handler.Register(accountService)).Methods("POST")
-	router.HandleFunc("/auth/forgotpassword", handler.ForgotPassword(accountService)).Methods("POST")
-	router.HandleFunc("/auth/newpassword", handler.NewPassword(accountService)).Methods("POST")
-	router.HandleFunc("/auth/verify", handler.Verification(accountService)).Methods("POST")
+	router.HandleFunc("/auth/forgotPassword", handler.ForgotPassword(accountService)).Methods("POST")
+	router.HandleFunc("/auth/newPassword", handler.NewPassword(accountService)).Methods("POST")
+	router.HandleFunc("/auth/deleteAccount", handler.DeleteAccount(accountService)).Methods("POST")
+	router.HandleFunc("/auth/verify/{token}", handler.Verification(accountService)).Methods("GET")
 }
